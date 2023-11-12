5aab7b6fe65888cdd1106c8a56afd5dca31cb1ec
Horwell <horwellmv@gmail.com>
2023-11-12 02:40:03 -0300

    Componentes para login y navbar  spinner

--- DressMeApp/src/app/componentes/admin/navbar/navbar.component.ts
+++ DressMeApp/src/app/componentes/admin/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Router } from "@angular/router";
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(private auth: AngularFireAuth,private router: Router){}
+
+  logOut(){
+    console.log("funciona logout");
+    this.auth.signOut();
+    this.router.navigate(['/auth']);    
+  }
 
 }

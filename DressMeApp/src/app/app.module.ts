0c8e9168e7edcc7e01443ece818a7042767ca5da
Horwell <horwellmv@gmail.com>
2023-11-12 00:25:06 -0300

    ng add @angular/fire

--- DressMeApp/src/app/app.module.ts
+++ DressMeApp/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"pp3-proyecto","appId":"1:1016829113014:web:1d1a77f1b660b5d9cdac17","storageBucket":"pp3-proyecto.appspot.com","locationId":"southamerica-east1","apiKey":"AIzaSyDLkvdAUtikBW7ey-wPy6O9Q3H3k_MuXFc","authDomain":"pp3-proyecto.firebaseapp.com","messagingSenderId":"1016829113014","measurementId":"G-VDSZ8NQ93D"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]

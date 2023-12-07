f6e932a64b9d7e3e79d402ee0390f6a1b253f08d
Horwell <horwellmv@gmail.com>
2023-12-07 00:37:32 -0300

    Desarrolla la funcion de admin perfiles

--- DressMeApp/src/app/componentes/admin/perfiles/perfiles.component.ts
+++ DressMeApp/src/app/componentes/admin/perfiles/perfiles.component.ts
@@ -1,6 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { User, user } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Iperfil } from 'src/app/interfaces/iperfil';
+import { AuthService } from 'src/app/servicios/auth.service';
 
 @Component({
   selector: 'app-perfiles',
@@ -8,54 +14,59 @@ import { Observable } from 'rxjs';
   styleUrls: ['./perfiles.component.css']
 })
 export class PerfilesComponent {
+  formPerfil: FormGroup;
   private firestore: Firestore = inject(Firestore);
+  private authSevice: AuthService = inject(AuthService);
 
-  users$: Observable<UserProfile[]>;
-  //usersCollection: CollectionReference;
+  perfil: Iperfil = {
+    nombre: "",
+    email: "",
+    password: "",
+    admin: false,
+  }
 
-  usuarios$: Observable<Usuarios[]>;
+  perfiles$: Observable<Iperfil[]>;
 
-    constructor() {
-        // get a reference to the user-profile collection
-        const userProfileCollection = collection(this.firestore, 'users');
-        const usuariosCollection = collection(this.firestore, 'usuarios');
+  constructor(private fb: FormBuilder, private auth: AngularFireAuth, private router: Router) {
 
-        // get documents (data) from the collection using collectionData
-        this.users$ = collectionData(userProfileCollection) as Observable<UserProfile[]>;
-        this.usuarios$ = collectionData(usuariosCollection) as Observable<Usuarios[]>;
+    // get a reference to the user-profile collection
+    const perfilesCollection = collection(this.firestore, 'perfiles');
+
+    // get documents (data) from the collection using collectionData
+    this.perfiles$ = collectionData(perfilesCollection) as Observable<Iperfil[]>;
+
+    this.formPerfil = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      admin: [],
+    })
+  }
+
+  async crearPerfil() {
+    // Creamos usuario con mail y contraseña
+    const resp = await this.authSevice.registrarPerfil(this.formPerfil.value).catch(error => {
+      console.log(error);
+    });
+    if (resp) {
+      console.log('Exito a crear usuario => ', resp.user); // regresa el usuario => Pediente agregar el user al Iperfil como parametro 
+
+      // Preparamos el objeto Iperfil
+      this.perfil = this.formPerfil.value;
+      const path = 'perfiles';
+      const id = resp.user?.uid;
+      this.perfil.id = id;
+      console.log('Perfil creado con User => ', this.perfil)
+
+      // Cargamos el perfil en la Db
+      await this.authSevice.crearDocPerfil(this.perfil, path, id);
+      this.formPerfil.reset();
     }
+  }
 
-    pepe: Usuarios = {
-      
-      nombre: 'test1',
-      email: 'test1@gmail.com',
-      turno: new Date,
-      estado: true
-    }
-    
-
-
-
-    async addUsuario() {
-      //if (!usuario) return;
-      try {
-        const docRef = await addDoc(collection(this.firestore, 'usuarios'), this.pepe)
-      } catch (error) {
-        console.log('error en metodo addusuarios: ', error)
-      }
-    }
+  async eliminarPerfil(perfil: Iperfil) {
+    const resp = await this.authSevice.borrarPerfil(perfil);
+    console.log('Exito al borrar perfil => ', resp);
+  }
 
 }
-
-export interface UserProfile {
-  username: string;
-}
-
-export interface Usuarios {
-  id?: string;
-  nombre: string;
-  email: string;
-  turno: Date;
-  estado: boolean;
-}
-
